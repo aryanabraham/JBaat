cd388de47e4efa0b2e8a390a8388a96b4710153e
Aryan Abraham <abraham.aryan01@gmail.com>
2023-03-28 02:52:19 +0530

    Finalized Login and Register page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 import Register from "./pages/Register";
 import "./style.scss";
 function App() {
-  return <Register />;
+  return <Home />;
 }
 
 export default App;
