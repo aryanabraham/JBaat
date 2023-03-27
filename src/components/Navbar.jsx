66e9fb331f57c9f254556cbc3eae914bf8f52aa9
Aryan Abraham <abraham.aryan01@gmail.com>
2023-03-28 03:28:28 +0530

    Create Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <span className="logo">JBaat!</span>
+      <div className="user">
+        <img src="" alt="" />
+        <span>Aryan</span>
+        <button>Logout</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
