4cd785b107eecdbaa6082c92a68c1d20535be4d6
Aryan Abraham <abraham.aryan01@gmail.com>
2023-03-28 17:48:45 +0530

    Finalized layout

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,19 @@
 import React from "react";
-
+import pp from "../img/pp-blue.png";
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <div className="search">
+      <div className="searchForm">
+        <input type="text" placeholder="Search or start a new chat" />
+      </div>
+      <div className="userChat">
+        <img src={pp} alt="profilepic" />
+        <div className="userChatInfo">
+          <span>Abraham</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
