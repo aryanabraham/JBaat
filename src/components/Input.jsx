b7628657733315b92928d8cd7723c07ed604e212
Aryan Abraham <abraham.aryan01@gmail.com>
2023-03-29 03:17:36 +0530

    Created message box and icons

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,20 @@
 import React from "react";
-
+import Img from "../img/image.png";
+import Attach from "../img/attach-file.png";
 const Input = () => {
-  return <div>Input</div>;
+  return (
+    <div className="input">
+      <input type="text" placeholder="Type a message" />
+      <div className="send">
+        <img src={Attach} alt="" />
+        <input type="file" style={{ display: "none" }} id="file" />
+        <label htmlFor="file">
+          <img src={Img} alt="" />
+        </label>
+        <button>Send</button>
+      </div>
+    </div>
+  );
 };
 
 export default Input;
