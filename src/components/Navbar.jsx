82b2d235a95f09deb0d2a6411480cc5fb44b054e
Aryan Abraham <abraham.aryan01@gmail.com>
2023-03-28 17:46:31 +0530

    Finalized layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-
+import pp from "../img/pp.jpg";
 const Navbar = () => {
   return (
     <div className="navbar">
       <span className="logo">JBaat!</span>
       <div className="user">
-        <img src="" alt="" />
+        <img src={pp} alt="profilepicture" />
         <span>Aryan</span>
         <button>Logout</button>
       </div>
