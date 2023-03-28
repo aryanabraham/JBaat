2728efd87d351e1b19ee3d2941bd6f4e95498eac
Aryan Abraham <abraham.aryan01@gmail.com>
2023-03-28 17:47:09 +0530

    Added dummy chats structure

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,7 +1,38 @@
 import React from "react";
-
+import pp from "../img/pp-blue.png";
 const Chats = () => {
-  return <div>Chats</div>;
+  return (
+    <div className="chats">
+      <div className="userChat">
+        <img src={pp} alt="profilepic" />
+        <div className="userChatInfo">
+          <span>Abraham</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img src={pp} alt="profilepic" />
+        <div className="userChatInfo">
+          <span>Abraham</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img src={pp} alt="profilepic" />
+        <div className="userChatInfo">
+          <span>Abraham</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img src={pp} alt="profilepic" />
+        <div className="userChatInfo">
+          <span>Abraham</span>
+          <p>Hello</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chats;
