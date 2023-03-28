43510c90c15aa910985b41658602f33c378f593c
Aryan Abraham <abraham.aryan01@gmail.com>
2023-03-29 03:25:52 +0530

    Add icons for voice/video call and more

--- src/components/MainChat.jsx
+++ src/components/MainChat.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import Video from "../img/video.png";
+import Call from "../img/telephone.png";
+import More from "../img/more.png";
+import Messages from "../components/Messages";
+import Input from "../components/Input";
 
 const MainChat = () => {
-  return <div className="mainchat">MainChat</div>;
+  return (
+    <div className="mainchat">
+      <div className="chatInfo">
+        <span>Abraham</span>
+        <div className="chatIcons">
+          <img src={Video} alt="" />
+          <img src={Call} alt="" />
+          <img src={More} alt="" />
+        </div>
+      </div>
+      <Messages />
+      <Input />
+    </div>
+  );
 };
 
 export default MainChat;
